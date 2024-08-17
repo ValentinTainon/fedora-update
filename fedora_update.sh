bcd12cb98ab4263a9d1fa3f328b372968aaf1bf6
ValentinTainon <valmerax@proton.me>
2024-08-17 16:39:51 +0200

    add bios check-update to firmwares section

--- fedora_update.sh
+++ fedora_update.sh
@@ -12,9 +12,9 @@ RESET_CLR="\e[0m"
 ### FUNCTIONS ###
 check_cmd() {
     if [[ $? -eq 0 ]]; then
-        echo -e "[$GREEN OK $RESET_CLR]"
+        echo -e "[${GREEN} OK ${RESET_CLR}]"
     else
-        echo -e "[$RED ERROR $RESET_CLR]"
+        echo -e "[${RED} ERROR ${RESET_CLR}]"
         exit 1
     fi
 }
@@ -23,11 +23,29 @@ check_cmd() {
 is_update_available="false"
 pkg_need_reboot=""
 
-### FIRMWARES ###
+### BIOS & FIRMWARES ###
+bios_device_version=$(sudo dmidecode -t bios | grep -Po '(?<=Version: )\d+')
+bios_web_version=1663
+
+if [[ $bios_device_version -lt $bios_web_version ]]; then
+    is_update_available="true"
+    bios_web_url="https://rog.asus.com/fr/motherboards/rog-strix/rog-strix-b760-i-gaming-wifi-model/helpdesk_bios/"
+
+    echo -e "${MAGENTA}BIOS:${RESET_CLR}"
+
+    echo -e "New BIOS version available! Download it from:
+    ${CYAN}${bios_web_url}${RESET_CLR}
+    " | sed 's/^[ \t]*//'
+
+    echo -e "${YELLOW}Press enter to continue executing the script...${RESET_CLR}"
+    read response
+fi
+
+
 if ! fwupdmgr get-updates 2>&1 | grep -q "No updates available"; then
     is_update_available="true"
 
-    echo -e "${MAGENTA}FIRMWARES : $RESET_CLR"
+    echo -e "${MAGENTA}FIRMWARES:${RESET_CLR}"
 
     sudo fwupdmgr update
 fi
@@ -38,11 +56,11 @@ dnf check-update >/dev/null 2>&1
 if [[ $? -eq 100 ]]; then
     is_update_available="true"
 
-    echo -e "${MAGENTA}RPM : $RESET_CLR"
+    echo -e "${MAGENTA}RPM:${RESET_CLR}"
 
     sudo dnf update
 
-    if  [[ $? -eq 0 ]]; then
+    if [[ $? -eq 0 ]]; then
         pkg_need_reboot=$(dnf history info last | grep -Eo '(dbus-broker|glibc|kernel|linux-firmware|systemd)' | sort | uniq | xargs)
     fi
 fi
@@ -51,7 +69,7 @@ fi
 if ! flatpak update | grep -q "Nothing to do"; then
     is_update_available="true"
 
-    echo -e "${MAGENTA}FLATPAK : $RESET_CLR"
+    echo -e "${MAGENTA}FLATPAK:${RESET_CLR}"
 
     flatpak update
 fi
@@ -63,9 +81,9 @@ xampp_web_version=$(curl -s 'https://www.apachefriends.org/fr/index.html' | grep
 if [[ $xampp_device_version -lt $xampp_web_version ]]; then
     is_update_available="true"
 
-    echo -e "${MAGENTA}XAMPP : $RESET_CLR"
+    echo -e "${MAGENTA}XAMPP:${RESET_CLR}"
     
-    echo -ne "${YELLOW}New version available! Do you want to update? [y/N] : $RESET_CLR"
+    echo -ne "${YELLOW}New version available! Do you want to update? [y/N] : ${RESET_CLR}"
     read response
 
     if [ "$response" == "y" ]; then
@@ -99,7 +117,7 @@ if [ "$is_update_available" == "false" ]; then
 fi
 
 if [[ -n "$pkg_need_reboot" ]]; then
-    echo -ne "${YELLOW}Restart required: ${CYAN}$pkg_need_reboot ${YELLOW}has been updated, restart now? [y/N] : $RESET_CLR"
+    echo -ne "${YELLOW}Restart required: ${CYAN}$pkg_need_reboot ${YELLOW}has been updated, restart now? [y/N] : ${RESET_CLR}"
     read response
     
     if [ "$response" == "y" ]; then
